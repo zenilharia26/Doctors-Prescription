b035b2270b4c00842d391311e4c014b467552f96
Smit Vora <smitmvora@gmail.com>
2020-01-30 05:20:41 +0530

    frontend+backend

--- server/cleanser.py
+++ server/cleanser.py
@@ -1,8 +1,6 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session,flash
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-
-
 import re
 import nltk
 from nltk.corpus import stopwords
@@ -17,15 +15,30 @@ class docregister(db.Model):
     password=db.Column(db.String(100),unique=True)
     contact=db.Column(db.String(100),unique=True)
 
+
     def __init__(self,name,email,password,contact):
         self.name=name
         self.email=email
         self.password=password
         self.contact=contact
+        
     def __ret__(self):
         return '<User %r>' % self.username   
-
-
+class patient(db.Model):
+    patient_id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(100))
+    symptoms=db.Column(db.String(1000))
+    diagnosis=db.Column(db.String(1000))
+    prescription=db.Column(db.String(2000))
+    docpres=db.Column(db.String(100))
+    def __init__(self,name,symptoms,diagnosis,prescription,docpres):
+        self.name=name
+        self.symptoms=symptoms
+        self.diagnosis=diagnosis
+        self.prescription=prescription
+        self.docpres=docpres
+        
+   
 # nltk.download('stopwords')
 # nltk.download('punkt')
 # nltk.download('averaged_perceptron_tagger')
@@ -98,81 +111,87 @@ def trial():
 def record():
     return render_template('js.html')
 
-# @app.route('/doctorsignup',methods = ['POST', 'GET'])
-# @app.route('/doctorlogin',methods = ['POST', 'GET'])
-# def doctorlogin():
-# 	if "email" in session:
-# 		data=[session["email"],session["password"]]
-# 		return redirect(url_for('index'))
-# 	elif request.method=="POST":
-# 		if 'doctorlogin' in request.form:
-# 			email=request.form['email']
-# 			password=request.form['password']
-# 			cur.execute("SELECT * FROM doctors WHERE email=%s AND password=crypt(%s,password);",(email,password,))
-# 			data=cur.fetchone()
-# 			#cur.close()
-# 			#con.close()
-# 			print(data)
-# 			if data:
-# 				session["fullname"]=data[0]
-# 				session["phoneno"]=data[1]
-# 				session["email"]=data[2]
-# 				session["password"]=data[3]
-# 				session["certificate"]=data[4]
-# 				flash('Logged in successfully! Welcome Dr. '+session["fullname"],'success')
-# 				return redirect(url_for('index'))
-# 			else:
-# 				flash('Invalid username or password. Please try again ','warning')
-# 				return render_template('doctorlogin.html',doctorlogin='active-link')
-        
-#         elif 'doctorsignup' in request.form:
-#             fullname=request.form['fullname']
-#             phoneno=request.form['phoneno']
-#             email=request.form['email']
-#             password=request.form['password']
-#             certificate=request.form['certificate']
-#             print(request.form)
-#             cur.execute("INSERT INTO doctors(fullname,phoneno,email,password,certificate) VALUES(%s,%s,%s,crypt(%s,gen_salt('bf')),%);",(fullname,phoneno,email,password,certificate,))
-#             con.commit()
-#             # cur.execute("INSERT INTO regusers VALUES(%s,0);",(uname,))
-#             # con.commit()
-#             #cur.close()
-#             #con.close()
-#             flash('Registration successful! Please login to continue','success')
-#             return render_template('doctorlogin.html',doctorlogin='active-link')
-# 	return render_template('doctorlogin.html',doctorlogin='active-link')
 
-# @app.route('/logout',methods = ['POST', 'GET'])
-# def logout():
-#     session.pop("fullname")
-#     session.pop("phoneno")
-#     session.pop("email")
-#     session.pop("password")
-#     session.pop("certificate")
-#     flash('Logged out successfully','success')
-#     return redirect(url_for('index'))    
 
 @app.route('/newuser')
 def index():
-    return render_template("register.html")
+    if 'name' in session:
+        return render_template("search.html",name=session['name'])
+    else:
+        return render_template("register.html")
+
+@app.route('/')
+def dashboard():
+    return render_template('register.html')
+
 @app.route('/register',methods=['POST'])
 def adding():
     if request.method=='POST':
-         user=docregister(request.form.get('namy'),request.form.get('email'),request.form.get('password'),request.form.get('contact'))
+         
+         user=docregister(request.form.get('name'),request.form.get('email'),request.form.get('password'),request.form.get('contact'))
          print(request.form)    
          db.session.add(user)   
          db.session.commit()
          return(redirect(url_for("loginuser")))
+
 @app.route('/login')
 def loginuser():
-    return render_template('login.html')         
+    if session:
+        return render_template("search.html",name=session['name'])
+    else:
+        return render_template("login.html")        
 
+@app.route('/validate',methods=['POST'])
+def validation():
+        if request.method=='POST':
+            emails=request.form.get('email')
+            passwords=request.form.get('password')
+            user=docregister.query.filter_by(email=emails).first()
+            if user:
+                if user.password==passwords:
+                    session['name']=user.name
+                    print(session['name'])  
+                    return render_template('search.html',name=session['name'])
+            flash("username or password not found","danger")
+            return (redirect(url_for('loginuser')))        
+             
+@app.route('/logout')
+def logout():
+        if 'name' in session:
+            session.pop('name',None)
+            return render_template('register.html')
 
+@app.route('/record')
+def speech():
+    if 'name' in session:
+        return render_template('speech.html',name=session['name'])
+    else:
+       return (redirect(url_for('loginuser')))     
+@app.route('/addpatient',methods=['POST'])
+def addpatients():
+    if  session:
+        if request.method=='POST':
+             user1=patient(request.form.get('name'),request.form.get('symptoms'),request.form.get('diagnosis'),request.form.get('prescription'),session['name'])
+             print(request.form.get('symptoms'))
+             print(session['name'])
+             print(request.form.get('diagnosis'))
+             db.session.add(user1)   
+             db.session.commit() 
+             return(redirect(url_for("speech")))
+    else:
+       return (redirect(url_for('loginuser')))
+@app.route('/search',methods=['POST'])
+def search():
+    if request.method=='POST':
 
-
+        ser=form.request.get('search')
+        find=patients.query.filter(patients.name.ilike(ser)).all()
+        for row in find:
+            print(row)
 
 if __name__ == '__main__':
     app.debug = True
+    app.secret_key="384tyhhfhr"
     app.run()
     
     
