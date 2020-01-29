765360248944c31702c5874b06da7a3b04cfde2f
Zenil Haria <zenil.haria@somaiya.edu>
2020-01-29 11:29:25 +0530

    Data extracted from voice.

--- server/cleanser.py
+++ server/cleanser.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 import re
@@ -15,10 +15,9 @@ stop = stopwords.words('english')
 tags = ['name']
 string = "Zenil Haria"
 
-def extract_phone_numbers(string):
-    r = re.compile(r'(\d{10})')
-    phone_numbers = r.findall(string)
-    return [re.sub(r'\D', '', number) for number in phone_numbers]
+def extract_phone_numbers(data):
+    data = data.replace(' ','')
+    return data
 
 def extract_email_addresses(string):
     r = re.compile(r'[\w\.-]+@[\w\.-]+')
@@ -39,31 +38,31 @@ def extract_names(document):
             if tag[1] in ['NNS', 'NNP', 'NN'] and tag[0] not in tags:
                 names.append(tag[0])
     print(names)
-    return names[0]
+    data = ''
+    for word in names:
+        data += word + ' '
+    return data
 
 
-@app.route('/name')
-def nameresolver():
-    names = extract_names(string)
-    print(names)
-    return names
-
-@app.route('/age')
-def ageresolver():
-    return "Heloo world"
-
-@app.route('/medicines')
-def medicinesresolver():
-    return "dont be high"
-
-@app.route('/advice')
-def adviceresolver():
-    return "be down to earth"
-
-@app.route('/number')
-def phoneresolver():
-    numbers = extract_phone_numbers(string)
-    return numbers[0]
+@app.route('/extract', methods=['POST'])
+def extract():
+    default_msg = 'Name nai aya'
+    if request.method == 'POST':
+        keyword= request.form['type']
+        data = request.form['data']
+        print(keyword)
+        print(data)
+        if keyword == 'name':
+            result = extract_names(data)
+        elif keyword == 'phone':
+            result = extract_phone_numbers(data)
+        elif keyword == 'email':
+            result = extract_email_addresses(data)
+        else:
+            result = 'Gand mara'
+        print(result)
+        return result
+    return default_msg
 
 @app.route('/email')
 def emailresolver():
@@ -79,6 +78,5 @@ def record():
     return render_template('js.html')
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run()
+    app.run(debug=True)
     
